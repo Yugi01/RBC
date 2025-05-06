c21bf89282eddaf8654ed6e7b58d0e5b9147de4f
Luka <yuks.zatezalo@gmail.com>
2025-05-06 16:32:20 +0200

    added the option for psuedo castling and then got all psudo illegal moves

--- main.py
+++ main.py
@@ -1,25 +1,48 @@
 import chess
+from reconchess.utilities import moves_without_opponent_pieces 
+from reconchess.utilities import is_illegal_castle
+from reconchess.utilities import is_psuedo_legal_castle
+from reconchess.utilities import without_opponent_pieces
 
 fen_input = input()
 
 board = chess.Board(fen_input)
 
+
+def get_pseudo_legal_castling(board):
+    castle_moves = []
+    if(chess.WHITE):
+        if bool(board.castling_rights & chess.BB_H1):
+            castle_moves.append("e1g1")
+        if bool(board.castling_rights & chess.BB_A1):
+            castle_moves.append("e1c1")
+    else:
+        if bool(board.castling_rights & chess.BB_H8):
+            castle_moves.append("e8g8")
+        if bool(board.castling_rights & chess.BB_A8):
+            castle_moves.append("e8c8")
+    return castle_moves
+
+def get_pl_castle_moves(board):
+    castle_moves = get_pseudo_legal_castling(without_opponent_pieces(board))
+    return castle_moves
+
 def exec_move(move):
     board.push(chess.Move.from_uci(move))
 
-def get_moves(fen):
+def get_moves(board):
     null_move = ["0000"]
     pl_moves = [str(move) for move in board.pseudo_legal_moves]
-    pl_moves.sort()
-    all_moves = null_move + pl_moves
+    pl_castle = get_pl_castle_moves(board)
+    all_moves = null_move + pl_castle + pl_moves
+    all_moves.sort()
     return all_moves
-
 
 # print(board)
 # move = input()
 # exec_move(move)
 # print(board.fen())
-all_moves = get_moves(board.fen())
+all_moves = get_moves(board)
 
 for move in all_moves:
     print(move)
