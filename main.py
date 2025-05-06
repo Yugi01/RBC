6004ae2df45f8ec9c2d3fcef9189b8a5f931c5f1
Luka <yuks.zatezalo@gmail.com>
2025-05-06 14:51:06 +0200

    set up board rep using the chess package

---
+++ main.py
@@ -0,0 +1,14 @@
+import chess
+
+fen_input = input()
+
+board = chess.Board(fen_input)
+
+def exec_move(move):
+    board.push(chess.Move.from_uci(move))
+
+
+print(board)
+# move = input()
+# exec_move(move)
+# print(board.fen())
