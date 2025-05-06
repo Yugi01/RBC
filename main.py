328a95b2f039fbaa17717b9c05a5f5933b6aa76f
Luka <yuks.zatezalo@gmail.com>
2025-05-06 15:17:50 +0200

    added method to get all pseudo legal moves

--- main.py
+++ main.py
@@ -7,8 +7,16 @@ board = chess.Board(fen_input)
 def exec_move(move):
     board.push(chess.Move.from_uci(move))
 
+def get_moves(fen):
+    null_move = ["0000"]
+    pl_moves = [str(move) for move in board.pseudo_legal_moves]
+    pl_moves.sort()
+    all_moves = null_move + pl_moves
+    return all_moves
+
 
 # print(board)
-move = input()
-exec_move(move)
-print(board.fen())
+# move = input()
+# exec_move(move)
+# print(board.fen())
+print(get_moves(board.fen()))
